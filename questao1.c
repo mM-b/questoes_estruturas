c07cc8a95dba7ae3c7f498be9613e001d8f12612
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-25 01:33:50 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct funcionarios
+typedef struct funcionarios // criando uma estrutura para funcionarios
 {
     char nome[50];
     float salarios;
@@ -10,6 +10,12 @@ typedef struct funcionarios
     char cargo[50];
 } Funcionario;
 int contador;
+
+/***
+ * função que lê as variaveis de funcionario
+ * @param *funcionarios
+ * @param tamanho
+ */
 void recebe(Funcionario *funcionarios, int tamanho)
 {
     for (contador = 0; contador < tamanho; contador++)
@@ -25,6 +31,11 @@ void recebe(Funcionario *funcionarios, int tamanho)
     }
 }
 
+/***
+ * função para imprimir os dados do funcionario
+ * @param *funcionarios
+ * @param tamanho
+ */
 void imprime(Funcionario *funcionarios, int tamanho)
 {
     for (contador = 0; contador < tamanho; contador++)
@@ -33,6 +44,10 @@ void imprime(Funcionario *funcionarios, int tamanho)
     }
 }
 
+/***
+ * função para alterar o salario de um funcionario
+ * @param *funcionarios
+ */
 void altera_sal(Funcionario *funcionarios)
 {
     printf("informe o novo salario");
@@ -41,24 +56,29 @@ void altera_sal(Funcionario *funcionarios)
     funcionarios->salarios = nv_sal;
 }
 
+/**
+ * função que descobre o maior e o menor salario
+ * @param *funcionarios
+ * @param qnt_funcionarios
+ */
 void salarios(Funcionario *funcionarios, int qnt_funcionarios)
 {
-    float maior = funcionarios[0].salarios, menor = funcionarios[0].salarios;
-    int mmaior = 0, mmenor = 0;
+    float maior = funcionarios[0].salarios, menor = funcionarios[0].salarios; // atribui a primeira posição de funcionarios o maior e o menor salario
+    int mmaior = 0, mmenor = 0;                                               // inicia os registradores de indice em 0, mmaior registra o indice do maior salario, e mmenor o indice do menor salario
 
-    for (contador = 0; contador < qnt_funcionarios; contador++)
+    for (contador = 0; contador < qnt_funcionarios; contador++) // laço for para percorrer todas as posições de funcionarios
     {
 
-        if (funcionarios[contador].salarios > maior)
+        if (funcionarios[contador].salarios > maior) // verifica se o salario do funcionario na posição contador é maior que o salario armazenado em maior
         {
-            maior = funcionarios[contador].salarios;
-            mmaior = contador;
+            maior = funcionarios[contador].salarios; // quando a condição do if é verdadeira maior é atualizado
+            mmaior = contador;                       // o indice do maior salario é registrado
         }
-        if (funcionarios[contador].salarios < menor)
+        if (funcionarios[contador].salarios < menor) // verifica se o salario do funcionario na posição contador é menor que o salario armazenado em menor
         {
-            menor = funcionarios[contador].salarios;
-            mmenor = contador;
-        }
+            menor = funcionarios[contador].salarios; // quando a condição do if é verdadeira menor é atualizado
+            mmenor = contador;                       // o indice do menor salario é registrado
+        }                                            // os indices são salvos para impressão do cargo do funcionario com o maior ou menor salario
     }
     printf("\nmaior salario:%.2f\ncargo do maior salario:%s\n\nmenor salario:%.2f\ncargo do menor salario:%s\n", maior, funcionarios[mmaior].cargo, menor, funcionarios[mmenor].cargo);
 }
@@ -68,8 +88,11 @@ int main(void)
     int tamanho;
     printf("informe a quantidade de funcionarios\n");
     scanf("%d", &tamanho);
-    Funcionario *funcionarios = (Funcionario *)malloc(tamanho * sizeof(Funcionario));
-
+    Funcionario *funcionarios = (Funcionario *)malloc(tamanho * sizeof(Funcionario)); // aloca dinamicamente um vetor de estruturas
+    if (funcionarios == NULL)
+    { // se a alocação foi mal sucedida a execução do programa é abortada
+        exit(1);
+    }
     recebe(funcionarios, tamanho);
 
     imprime(funcionarios, tamanho);
@@ -82,6 +105,6 @@ int main(void)
 
     altera_sal(&funcionarios[funcio - 1]);
     printf("novo salario do funcionario %d:%.2f", funcio, funcionarios[funcio - 1].salarios);
-    free(funcionarios);
+    free(funcionarios); // libera a memoria alocada por funcionarios
     return 0;
 }
